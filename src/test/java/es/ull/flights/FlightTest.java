d1a3fb54ac2b7723749289865723a4a8b5392da5
Nahia Ganuza <72313513+NahiaGanuza@users.noreply.github.com>
2023-12-02 11:21:26 +0000

    Probando la excepcion de un codigo de vuelo invalido

--- src/test/java/es/ull/flights/FlightTest.java
+++ src/test/java/es/ull/flights/FlightTest.java
@@ -22,6 +22,11 @@ public class FlightTest {
 
     @Test
     public void testConstructorValidFlightNumber() {
+        assertThrows(RuntimeException.class(), () -> new Flight("ABD123", 100));
+    }
+
+    @Test
+    public void testConstructorValidFlightNumber() {
         String validFlightNumber = "AB123";
         int seats = 100;
 
